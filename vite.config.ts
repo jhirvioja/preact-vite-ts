2606b35f659feab74f7ebd83157f300e38a795d3
jhirvioja <jhirvioja@gmail.com>
2023-04-30 17:02:51 +0300

    Try without base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.PROD ? '/preact-vite-ts/' : '',
+  // base: process.env.PROD ? '/preact-vite-ts/' : '',
   plugins: [preact()],
 })
